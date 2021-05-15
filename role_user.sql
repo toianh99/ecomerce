7102a8e570213c5259d54de068cc8e9d69859a15
toianh99 <toileanh84@gmail.com>
2021-05-15 23:34:21 +0700

    update base

---
+++ role_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO ecomerce_laravel.role_user (user_id, role_id) VALUES (1, 1);
---
+++ roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO ecomerce_laravel.roles (id, title, description, created_at, updated_at) VALUES (1, 'test', null, '2021-05-01 10:32:40', '2021-05-01 10:32:41');
+INSERT INTO ecomerce_laravel.roles (id, title, description, created_at, updated_at) VALUES (2, 'test1', null, '2021-05-01 15:06:52', '2021-05-01 15:06:52');
