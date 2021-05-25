5a7a814e99b3dfa3680bcb50086b94f6989057fd
toianh99 <toileanh84@gmail.com>
2021-05-25 23:21:35 +0700

    update base

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RedStore | Ecommerce Website Design</title>
-    <link rel="stylesheet" href="{{secure_secure_asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
