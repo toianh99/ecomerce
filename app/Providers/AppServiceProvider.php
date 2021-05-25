e375c1a12d54e12772f3943d7cf512d882cde1e4
toianh99 <toileanh84@gmail.com>
2021-05-25 21:18:18 +0700

    update base

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('paginator.pagination');
+        if (env('APP_ENV')!=='local'){
+            URL::forceScheme('https');
+        }
     }
 }
